602c0fa09c314149dcfbb779f94c42160ab9dc34
mar1nel <vicu-marin.ababii@student.upt.ro>
2024-04-16 03:33:50 +0300

    Class Select Page UI done

--- rpg-frontend/src/pages/ClassSelectPage.tsx
+++ rpg-frontend/src/pages/ClassSelectPage.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ImageButtons from "../components/ClassSelectComponent";
+import WhiteButton from "../components/WhiteButton";
 import Layout from "../Layout";
 import "./ClassSelectPage.css";
+
+
 const ClassSelectPage: React.FC = () => {
 
     const handleSelectClass = (className: string) => {
         console.log("User selected:", className);
     };
 
+    const handleClick = () => {
+        console.log("Button clicked!");
+    };
 
     return (
         <Layout showLogo={true}>
@@ -17,8 +23,14 @@ const ClassSelectPage: React.FC = () => {
             </div>
 
           <div>
-               <div style={{maxWidth:1000,marginLeft: "auto", marginRight: "auto"}}>
+               <div style={{maxWidth:1000,marginLeft: "auto", marginTop: 30, marginRight: "auto"}}>
                   <ImageButtons onSelectClass={handleSelectClass} />
+              </div>
+              <div className="down-buttons">
+                  <WhiteButton onClick={handleClick} text="GITHUB REPO" />
+                  <WhiteButton onClick={handleClick} text="ABOUT PROJECT" width="160px"/>
+                  <WhiteButton onClick={handleClick} text="CLASS INFO" />
+                  <WhiteButton onClick={handleClick} text="CONFIRM" />
               </div>
           </div>
 
